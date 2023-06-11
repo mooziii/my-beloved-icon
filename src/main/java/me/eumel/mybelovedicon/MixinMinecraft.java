2a22f234cc1175d444b10c228b820892faae7501
mooz <63669478+mooziii@users.noreply.github.com>
2023-06-11 11:42:42 +0200

    1.19.4 and lower!

--- src/main/java/me/eumel/mybelovedicon/MixinMinecraft.java
+++ src/main/java/me/eumel/mybelovedicon/MixinMinecraft.java
@@ -1,11 +1,13 @@
 package me.eumel.mybelovedicon;
 
-import com.mojang.blaze3d.platform.IconSet;
+import com.mojang.blaze3d.platform.MacosUtil;
 import com.mojang.blaze3d.platform.Window;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.main.GameConfig;
-import net.minecraft.server.packs.PackResources;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.VanillaPackResources;
+import net.minecraft.server.packs.resources.IoSupplier;
+import net.minecraft.server.packs.resources.ResourceManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Final;
@@ -15,23 +17,42 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.io.InputStream;
+
+import static net.minecraft.client.Minecraft.ON_OSX;
 
 @Mixin(Minecraft.class)
-public class MixinMinecraft {
+public abstract class MixinMinecraft {
 
     @Shadow @Final private Window window;
     @Shadow @Final private VanillaPackResources vanillaPackResources;
+
+    @Shadow public abstract ResourceManager getResourceManager();
+
     private static final Logger LOGGER = LoggerFactory.getLogger("my-beloved-icon");
 
-    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/Window;setIcon(Lnet/minecraft/server/packs/PackResources;Lcom/mojang/blaze3d/platform/IconSet;)V"))
-    private void init(Window instance, PackResources packResources, IconSet iconSet) {
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/MacosUtil;loadIcon(Lnet/minecraft/server/packs/resources/IoSupplier;)V"))
+    private void initMacOsIcon(IoSupplier<InputStream> ioSupplier) {
+        LOGGER.info("Bringing back your beloved icon since 2023!");
+    }
+
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/Window;setIcon(Lnet/minecraft/server/packs/resources/IoSupplier;Lnet/minecraft/server/packs/resources/IoSupplier;)V"))
+    private void initWindowsIcon(Window instance, IoSupplier<InputStream> ioSupplier, IoSupplier<InputStream> ioSupplier2) {
         LOGGER.info("Bringing back your beloved icon since 2023!");
     }
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void setIcon(GameConfig gameConfig, CallbackInfo ci) {
         try {
-            window.setIcon(vanillaPackResources, IconSet.RELEASE);
+            ResourceManager resourceManager = this.getResourceManager();
+            if (ON_OSX) {
+                MacosUtil.loadIcon(() -> resourceManager.getResourceOrThrow(new ResourceLocation("my-beloved-icon", "minecraft.icns")).open());
+            } else {
+                this.window.setIcon(
+                    () -> resourceManager.getResourceOrThrow(new ResourceLocation("my-beloved-icon", "icon_16x16.png")).open(),
+                    () -> resourceManager.getResourceOrThrow(new ResourceLocation("my-beloved-icon", "icon_32x32.png")).open()
+                );
+            }
         } catch (Exception e) {
             LOGGER.error("Could not set icon :(", e);
         }
